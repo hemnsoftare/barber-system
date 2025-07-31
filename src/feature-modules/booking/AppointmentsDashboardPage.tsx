3bdf5dad80c5e2699c476f4482c076b38bd881d9
hemnsoftare <138198540+hemnsoftare@users.noreply.github.com>
2025-07-31 14:43:39 +0300

    .

--- src/feature-modules/booking/AppointmentsDashboardPage.tsx
+++ src/feature-modules/booking/AppointmentsDashboardPage.tsx
@@ -5,8 +5,10 @@ import AppointmentTable from "./components/AppointmentTable";
 import { redirect } from "next/navigation";
 import { useUser } from "@clerk/nextjs";
 import { toast } from "sonner";
+import { useSelectedAppointment } from "../users/store";
 const AppointmentsDashboardPage = () => {
   const { user } = useUser();
+  const { clearSelected } = useSelectedAppointment();
   const role = user?.publicMetadata.role as "admin" | "barber";
   return (
     <div>
@@ -17,6 +19,7 @@ const AppointmentsDashboardPage = () => {
               toast.error("You are not allowed to create appointments");
               return;
             }
+            clearSelected();
             redirect("/dashboard/appointments/create");
           }}
           className="bg-dark-purple text-nowrap px-3 md:hover:bg-dark-purple/90 transition-all duration-300 active:scale-95 sm:px-6 py-1 text-white"
