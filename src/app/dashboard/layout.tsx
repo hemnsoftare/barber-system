ba06384c13b5b85d87d3ed55712780327118dc5f
hemnsoftare <138198540+hemnsoftare@users.noreply.github.com>
2025-08-03 18:05:18 +0300

    .

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,8 +1,13 @@
 // app/dashboard/layout.tsx
 
 import DashboardLayout from "@/feature-modules/dashboard/layout";
+import { Viewport } from "next";
 import type { ReactNode } from "react";
-
+export const viewport: Viewport = {
+  themeColor: "#1E1E1E",
+  width: "device-width",
+  initialScale: 1,
+};
 export default function Layout({ children }: { children: ReactNode }) {
   return <DashboardLayout>{children}</DashboardLayout>;
 }
